af7502511459748362fc7fbc31228f6411211d5d
ddienu <rockdann@hotmail.com>
2025-08-20 19:05:37 -0500

    feat: Adding parameter nickname into getPlayers in order to allow the user to search a player by its nickname

--- src/app/core/services/player.service.ts
+++ src/app/core/services/player.service.ts
@@ -8,6 +8,8 @@ import { JwtService } from './jwt.service';
 import { StorageService } from './storage.service';
 import { API_URL } from '../../tokens/api-url.token';
 import { ApiPath } from '../../shared/constants/api-paths';
+import { Player } from '../../features/players/model/player.model';
+import { Page } from '../../shared/models/page.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,27 +17,48 @@ import { ApiPath } from '../../shared/constants/api-paths';
 export class PlayerService {
 
   constructor(
-    private http:HttpClient, 
-    private jwtService:JwtService, 
-    private storageService:StorageService,
-    @Inject(API_URL) private apiUrl : string
-  ) {}
+    private http: HttpClient,
+    private jwtService: JwtService,
+    private storageService: StorageService,
+    @Inject(API_URL) private apiUrl: string
+  ) { }
 
-  getPlayerById(playerId:number):Observable<ApiResponse<PlayerResponse>>{
+  getPlayers(options?: { page?: number; size?: number; nickname?: string }): Observable<ApiResponse<Page<Player>>> {
+    const params: any = {};
+
+    if (options?.nickname) params.nickname = options.nickname;
+    if (options?.page !== undefined) params.page = options.page;
+    if (options?.size !== undefined) params.size = options.size;
+
+    return this.http.get<ApiResponse<Page<Player>>>(`${this.apiUrl}${ApiPath.player}`, { params });
+  }
+
+  getPlayerById(playerId: number): Observable<ApiResponse<PlayerResponse>> {
     return this.http.get<ApiResponse<PlayerResponse>>(`${this.apiUrl}${ApiPath.player}/${playerId}`);
   }
-  
-  getPlayerByUserId(userId : number) : Observable<ApiResponse<PlayerResponse>>{
+
+  getPlayerByUserId(userId: number): Observable<ApiResponse<PlayerResponse>> {
     // const userId = this.jwtService.getUserIdFromToken();
 
-    return this.http.get<ApiResponse<PlayerResponse>>(`${this.apiUrl}${ApiPath.player}/user/${userId}`)
+    return this.http.get<ApiResponse<PlayerResponse>>(`${this.apiUrl}${ApiPath.player}/user/${userId}`);
   }
 
-  createPlayer(playerPayload : CreatePlayer) : Observable<ApiResponse<CreatePlayer>>{
+  findPlayersByNickname(nickname: string): Observable<ApiResponse<Page<Player>>> {
+    const params = {
+      nickname: nickname
+    };
+    return this.http.get<ApiResponse<Page<Player>>>(`${this.apiUrl}${ApiPath.player}/search`, { params });
+  }
+
+  createPlayer(playerPayload: CreatePlayer): Observable<ApiResponse<CreatePlayer>> {
     return this.http.post<ApiResponse<CreatePlayer>>(`${this.apiUrl}${ApiPath.player}`, playerPayload);
   }
 
-  editPlayer(playerId : number, playerEditedPayload : CreatePlayer) : Observable<ApiResponse<CreatePlayer>>{
+  editPlayer(playerId: number, playerEditedPayload: CreatePlayer): Observable<ApiResponse<CreatePlayer>> {
     return this.http.put<ApiResponse<CreatePlayer>>(`${this.apiUrl}${ApiPath.player}/${playerId}`, playerEditedPayload);
+  }
+
+  deletePlayer(playerId: number): Observable<ApiResponse<any>> {
+    return this.http.delete<ApiResponse<any>>(`${this.apiUrl}${ApiPath.player}/${playerId}`);
   }
 }
